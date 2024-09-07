71d1ad31e6bb225defb04df50f21778bb8c1b32f
Ben <luben12337@gmail.com>
2024-09-07 23:45:22 +1000

    Created base code

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import ResumeForm from './components/ResumeForm'
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="content">
+      <ResumeForm />
+    </div>
+  )
+}
+
+export default App
